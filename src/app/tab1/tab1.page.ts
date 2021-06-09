ccfa8468be9104fcd78ca8f0b1de4c9d37e95dd7
José Antonio Gallo Junior <gallojunior@gmail.com>
2021-06-09 00:02:48 -0300

    Consumindo Generos da API The MovieDB e redefinindo as interfaces com os serviços e Models.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,10 @@
+import { IGenero } from './../models/IGenero.model';
+import { GeneroService } from './../services/genero.service';
 import { IListaFilmes, IFilmeApi } from './../models/IFilmeAPI.model';
 import { FilmeService } from './../services/filme.service';
 import { DadosService } from './../services/dados.service';
 import { IFilme } from '../models/IFilme.model';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
 import { Router } from '@angular/router';
@@ -12,7 +14,7 @@ import { Router } from '@angular/router';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
   titulo = 'Filmes';
 
   listaVideos: IFilme[] = [
@@ -41,34 +43,40 @@ export class Tab1Page {
       lancamento: '23/04/2021',
       duracao: '1h57',
       classificacao: 84,
-      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/m2FNRngyJMyxLatBMJR8pbeG2v.jpg',
-      generos: ['Animação', 'Ação', 'Aventura', 'Fantasia', 'Drama']
+      cartaz:
+        'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/m2FNRngyJMyxLatBMJR8pbeG2v.jpg',
+      generos: ['Animação', 'Ação', 'Aventura', 'Fantasia', 'Drama'],
     },
     {
       nome: 'Espiral: O Legado de Jogos Mortais (2021)',
       lancamento: '14/05/2021',
       duracao: '1h33',
       classificacao: 66,
-      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/3OneyM3YB6WPdvrfdixL17MXH3J.jpg',
-      generos: ['Thriller', 'Terror', 'Mistério']
+      cartaz:
+        'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/3OneyM3YB6WPdvrfdixL17MXH3J.jpg',
+      generos: ['Thriller', 'Terror', 'Mistério'],
     },
     {
       nome: 'Mortal Kombat (2021)',
       lancamento: '15/04/2021',
       duracao: '1h50',
       classificacao: 75,
-      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ijvC2w2yANsfgLT3LMu2zFr0fxh.jpg',
-      generos: ['Ação', 'Fantasia', 'Aventura']
+      cartaz:
+        'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ijvC2w2yANsfgLT3LMu2zFr0fxh.jpg',
+      generos: ['Ação', 'Fantasia', 'Aventura'],
     },
     {
       nome: 'New Gods Nezha Reborn (2021)',
       lancamento: '12/02/2021',
       duracao: '1h56',
       classificacao: 86,
-      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/iyOgNaaDX67AUMWlzsGZVO1luoh.jpg',
-      generos: ['Animação', 'Ação', 'Fantasia']
-    }
+      cartaz:
+        'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/iyOgNaaDX67AUMWlzsGZVO1luoh.jpg',
+      generos: ['Animação', 'Ação', 'Fantasia'],
+    },
   ];
+
+  generos: string[] = [];
 
   listaFilmes: IListaFilmes;
 
@@ -77,15 +85,17 @@ export class Tab1Page {
     public toastController: ToastController,
     public dadosService: DadosService,
     public filmeService: FilmeService,
+    public generoService: GeneroService,
     public route: Router
   ) {}
 
-  buscarFilmes(evento: any){
+  buscarFilmes(evento: any) {
     console.log(evento.target.value);
     const busca = evento.target.value;
-    if (busca && busca.trim() !== '')
-    {
-      this.filmeService.buscarFilmes(busca).subscribe(dados =>{this.listaFilmes = dados;});
+    if (busca && busca.trim() !== '') {
+      this.filmeService.buscarFilmes(busca).subscribe((dados) => {
+        this.listaFilmes = dados;
+      });
     }
   }
 
@@ -126,5 +136,14 @@ export class Tab1Page {
       position: 'middle',
     });
     toast.present();
+  }
+
+  ngOnInit() {
+    this.generoService.buscarGeneros().subscribe((dados) => {
+      dados.genres.forEach((genero) => {
+        this.generos[genero.id] = genero.name;
+      });
+      this.dadosService.guardarDados('generos', this.generos);
+    });
   }
 }
