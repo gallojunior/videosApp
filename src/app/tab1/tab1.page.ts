de6e831aaa1d01ea18661a19df85a7fa7bdb5bfb
José Antonio Gallo Junior <gallojunior@gmail.com>
2021-06-08 21:28:55 -0300

    Inclusão de novos filmes e mudança de classe na classificação.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -34,6 +34,38 @@ export class Tab1Page {
       generos: ['Ação', 'Aventura', 'Ficção científica'],
       pagina: '/godzilla-vs-king-kong',
     },
+    {
+      nome: 'Demon Slayer - Mugen Train: O Filme (2020)',
+      lancamento: '23/04/2021',
+      duracao: '1h57',
+      classificacao: 84,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/m2FNRngyJMyxLatBMJR8pbeG2v.jpg',
+      generos: ['Animação', 'Ação', 'Aventura', 'Fantasia', 'Drama']
+    },
+    {
+      nome: 'Espiral: O Legado de Jogos Mortais (2021)',
+      lancamento: '14/05/2021',
+      duracao: '1h33',
+      classificacao: 66,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/3OneyM3YB6WPdvrfdixL17MXH3J.jpg',
+      generos: ['Thriller', 'Terror', 'Mistério']
+    },
+    {
+      nome: 'Mortal Kombat (2021)',
+      lancamento: '15/04/2021',
+      duracao: '1h50',
+      classificacao: 75,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ijvC2w2yANsfgLT3LMu2zFr0fxh.jpg',
+      generos: ['Ação', 'Fantasia', 'Aventura']
+    },
+    {
+      nome: 'New Gods Nezha Reborn (2021)',
+      lancamento: '12/02/2021',
+      duracao: '1h56',
+      classificacao: 86,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/iyOgNaaDX67AUMWlzsGZVO1luoh.jpg',
+      generos: ['Animação', 'Ação', 'Fantasia']
+    }
   ];
 
   constructor(
